50d00b1dec437f3acf8d141d5923d021a5945f24
Joel Hogg <joelhogg45@gmail.com>
2024-09-30 20:45:24 +0100

    renamed todo_item to name

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@ export default {
     async getTodos() {
       try {
         const response = await axios.get('https://6lzzhlthsj.execute-api.eu-north-1.amazonaws.com/dev');
-        // If the response body is a stringified JSON, parse it
+        // If the response body is stringified JSON, parse it
         const data = JSON.parse(response.data.body);
 
         this.todos = data.items || [];  // Assuming the response structure is { "items": [...] }
@@ -45,15 +45,16 @@ export default {
     // Method to POST a new todo using Axios
     async addTodo() {
       try {
+        // Send the new todo item to the backend
         const response = await axios.post('https://6lzzhlthsj.execute-api.eu-north-1.amazonaws.com/dev', {
-          todo_item: this.newTodo   // Sending the new todo item in the expected format
+          name: this.newTodo   // The name field matches your Lambda's expectation
         }, {
           headers: {
-            'Content-Type': 'application/json' // Ensure proper headers are sent
+            'Content-Type': 'application/json'  // Ensure proper headers are sent
           }
         });
 
-        if (response.status === 200) { // Assuming 200 status for success (could be 201 if your API is set up that way)
+        if (response.status === 200) {  // Check for successful response
           this.newTodo = '';  // Reset the input field
           this.getTodos();    // Fetch the updated todo list after adding a new one
         } else {
